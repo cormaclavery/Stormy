383f4438c09a1c5536edad71c351f382f35ecb4d
cormaclavery <cormac.m.lavery@gmail.com>
2016-02-26 14:14:08 +0000

    first commit

---
+++ app/src/main/java/com/cormaclavery/stormy/UI/DailyForecastActivity.java
@@ -0,0 +1,13 @@
+package com.cormaclavery.stormy.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DailyForecastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+    }
+}
